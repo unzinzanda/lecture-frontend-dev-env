567892948a5a438633e2203303c76b1abb1048d7
unzinzanda <93910197+unzinzanda@users.noreply.github.com>
2024-04-02 00:31:11 +0900

    24.04.02 webpack_loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,29 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    main: './src/app.js'
+    main: "./src/app.js",
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve('./dist'),
-  }
-}
+    filename: "[name].js",
+    path: path.resolve("./dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: "url-loader",
+        options: {
+          publicPath: "./dist/",
+          name: "[name].[ext]?[hash]",
+          limit: 7000,
+        },
+      },
+    ],
+  },
+};
